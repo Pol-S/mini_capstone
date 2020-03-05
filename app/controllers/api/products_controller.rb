a09152b008ad81b28901064f585838ceaee19fe3
Pol Subanajouy <plsbnjy@gmail.com>
2020-03-04 18:37:56 -0600

    Got the segment params for individual pages to work.

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,28 +4,9 @@ class Api::ProductsController < ApplicationController
     render "all_products.json.jb"
   end
 
-  def product_id_1_method
-    @product1 = Product.find_by(id: 1)
-    render "product_id_1.json.jb"
-  end
-
-  def product_id_2_method
-    @product2 = Product.find_by(id: 2)
-    render "product_id_2.json.jb"
-  end
-
-  def product_id_3_method
-    @product3 = Product.find_by(id: 3)
-    render "product_id_3.json.jb"
-  end
-
-  def product_id_4_method
-    @product4 = Product.find_by(id: 4)
-    render "product_id_4.json.jb"
-  end
-
-  def product_id_5_method
-    @product5 = Product.find_by(id: 5)
-    render "product_id_5.json.jb"
+  def product_retrieve
+    @id_number = params["id_number"]
+    @product = Product.find_by(id: @id_number)
+    render "product_id_template.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/all_products" => "products#all_products"
-    get "/product_id_1" => "products#product_id_1_method"
-    get "/product_id_2" => "products#product_id_2_method"
-    get "/product_id_3" => "products#product_id_3_method"
-    get "/product_id_4" => "products#product_id_4_method"
-    get "/product_id_5" => "products#product_id_5_method"
+    # get "/product_id" => "products#product_retrive"
+    get "/product_id/:id_number" => "products#product_retrieve"
   end
 end
