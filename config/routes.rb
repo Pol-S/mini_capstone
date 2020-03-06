1c975af43162769cf12fefccdf97aacf93cfde96
Pol Subanajouy <plsbnjy@gmail.com>
2020-03-05 18:47:46 -0600

    buh?

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/products" => "products#index"
-    #   get "/products" => "products#product_retrieve"
-    #   get "/products/:id" => "products#product_retrieve"
+    # get "/products" => "products#product_retrieve"
+    get "/products/:id" => "products#product_retrieve"
   end
 end
