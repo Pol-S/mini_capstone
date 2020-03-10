23723ae16dfef6e50b244834005a4b91bb4590ad
Pol Subanajouy <plsbnjy@gmail.com>
2020-03-09 20:46:54 -0500

    some validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
