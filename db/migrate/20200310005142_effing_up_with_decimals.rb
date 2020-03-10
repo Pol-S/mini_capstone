168354e85c7cba008705a26666077563a241ef8d
Pol Subanajouy <plsbnjy@gmail.com>
2020-03-09 20:00:50 -0500

    purposely added a busted decimal migration missing parameters

---
+++ db/migrate/20200310005142_effing_up_with_decimals.rb
@@ -0,0 +1,5 @@
+class EffingUpWithDecimals < ActiveRecord::Migration[6.0]
+  def change
+    change_column :products, :price, :decimal
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_004421) do
+ActiveRecord::Schema.define(version: 2020_03_10_005142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price"
     t.string "image_url"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
