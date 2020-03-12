8520248bf534c3333d2255df92f1626248cad42f
Pol Subanajouy <plsbnjy@gmail.com>
2020-03-11 21:03:41 -0500

    got suppliers and products to show up in one another

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,10 @@ class Product < ApplicationRecord
   validates :name, uniqueness: true
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
+  
+  def supplier
+    Supplier.find_by(id: supplier_id)
+  end
 
   def tax
     price * 0.09
